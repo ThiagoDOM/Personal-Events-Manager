b9f57ec716b8b967fd8f2fb9e8331999f42c29e8
Thiago Moreira <thiagob7747@gmail.com>
2024-01-05 00:58:42 -0300

    User profiles defined

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        \App\Models\User::create([
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
+            'password' => bcrypt('admin'),
+            'role' => 'admin'
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
