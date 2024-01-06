d8e0aabc18ff22b830a3c923f1e3ebefe66942c6
Thiago Moreira <thiagob7747@gmail.com>
2024-01-06 16:54:43 -0300

    fix admins list

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -18,6 +18,7 @@ class AdminController extends Controller
     public function index(Request $request)
     {
         $user = User::query();
+        $user->admins();
         if($request->name)
             $user->where('name', 'LIKE', "%$request->name%");
 
